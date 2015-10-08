f0e20aabf45585a040fffc68eee6ad5e0504f219
Alex Goff <alexander.r.goff@gmail.com>
2015-10-08 15:06:40 -0400

    fixed more navbar links

--- contact/index.php
+++ contact/index.php
@@ -28,7 +28,7 @@
 
 <body>
 
-<?php include ('/tke/include/navbar.html'); ?>
+<?php include ('tke/include/navbar.html'); ?>
 	
 		<!-- Content -->
 		
--- news/index.php
+++ news/index.php
@@ -27,7 +27,7 @@
 
 <body>
 
-<?php include ('/tke/include/navbar.html'); ?>
+<?php include ('tke/include/navbar.html'); ?>
 
 <!-- Begin Body -->
 <div class="container">
