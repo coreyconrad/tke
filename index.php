2c45fdec03ebc040eba2e875bebd390023c84857
unknown <ag04536@GEOD2032030X016.ad.georgiasouthern.edu>
2015-10-08 14:52:57 -0400

    changed home page to PHP, added navbar as include, oh god I hope this doesn't break

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
     <title>Tau Kappa Epsilon - Lambda Upsilon</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/tke/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/tke/css/tke.css" rel="stylesheet">
+    <link href="css/tke.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,39 +28,7 @@
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-static-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/tke">
-					<img src="/tke/img/tke_logo.png">
-				</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="/tke/members">Members</a>
-                    </li>
-                    <li>
-                        <a href="/tke/news">News</a>
-                    </li>
-                    <li>
-                        <a href="/tke/contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<?php include ('include/navbar.html'); ?>
 
     <!-- Page Content -->
     <div class="container">
