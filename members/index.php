45bc239bc4e7f38dea7956367a8cc60ffa18709f
Alex Goff <alexander.r.goff@gmail.com>
2015-10-15 11:25:22 -0400

    changed font and updated member blurbs

--- members/index.php
+++ members/index.php
@@ -50,7 +50,7 @@ include_once($path);
 			<h3>Juho Lepparanta</h3>
 			<h4>Prytanis</h4>
 			<h6>Scroll Number: 574</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags. extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra </p>
+			<p></p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -59,7 +59,7 @@ include_once($path);
 			<h3>Jeremy Sklar</h3>
 			<h4>Epiprytanis</h4>
 			<h6>Scroll Number: 568</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Senior Information Technology major. Outside of TKE Jeremy enjoys video games and playing with his dog, Rocky.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -68,7 +68,7 @@ include_once($path);
 			<h3>Garrett Manley</h3>
 			<h4>Hypophetes</h4>
 			<h6>Scroll Number: 562</h6>
-			<p>Fourth Year Computer Science Major, past prytanis of the lambda-upsilon colony.</p>
+			<p>Fourth year Computer Science major, past prytanis of the Lambda-Upsilon Colony.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -77,7 +77,7 @@ include_once($path);
 			<h3>Chris Hamb</h3>
 			<h4>Pylortes</h4>
 			<h6>Scroll Number: 584</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p></p>
 		</div>
 	</div>
 	<hr />
@@ -89,7 +89,7 @@ include_once($path);
 			<h3>Blake Stalnaker</h3>
 			<h4>Histor</h4>
 			<h6>Scroll Number: 579</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Junior Construction Management major. Vice President of the CMAA and a member of Chi Alpha. Blake is a fan of all sports and loves camping and sleeping outside in his hammock.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -98,7 +98,7 @@ include_once($path);
 			<h3>Evan Clark</h3>
 			<h4>Hegemon</h4>
 			<h6>Scroll Number: 564</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Junior Spanish major. Along with Tau Kappa Epsilon Evan is involved with Chi Alpha, Association of Latin American Students, Greek Ambassadors, and Eagles Economics Club. A leader in Chi Alpha and treasurer of the Eagles Economics Club.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -107,7 +107,7 @@ include_once($path);
 			<h3>Samuel Mason</h3>
 			<h4>Crysophylos</h4>
 			<h6>Scroll Number: 565</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Junior Information Technology major at Georgia Southern.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -116,7 +116,7 @@ include_once($path);
 			<h3>Kyle Mantay</h3>
 			<h4>Grammateus</h4>
 			<h6>Scroll Number: 589</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>A hip, young sophomore Civil Engineering major. Likes to hang out with his brothers and playing baseball.</p>
 		</div>
 	</div>
 	<hr />
@@ -127,7 +127,7 @@ include_once($path);
 			</a>
 			<h3>Dylan John</h3>
 			<h6>Scroll Number: 566</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Senior Construction Management major and an Honors student with a thesis concentration on Building Information Modeling. Serves as a Senator for the College of Engineering and Information Technology on the Student Government Assocation and is involved in numerous student organizations including the World Championship Mock Mediation Club which he started at Georgia Southern University in 2013. Frater Dylan John serves as the recruitment chairman of the Lambda Upsilon Colony.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -143,7 +143,7 @@ include_once($path);
 			</a>
 			<h3>Thomas Jones</h3>
 			<h6>Scroll Number: 571</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Senior Computer Science major. Likes to work on computers and do other nerdy things like building robots.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -151,7 +151,7 @@ include_once($path);
 			</a>
 			<h3>Mathias Koeppel</h3>
 			<h6>Scroll Number: 581</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p></p>
 		</div>
 	</div>
 	<hr />
@@ -162,7 +162,7 @@ include_once($path);
 			</a>
 			<h3>Thomas Carden</h3>
 			<h6>Scroll Number: 583</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p></p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -170,7 +170,7 @@ include_once($path);
 			</a>
 			<h3>Michael Jenkins</h3>
 			<h6>Scroll Number: 587</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>Junior Accounting major from Brunswick, GA. Michael loves music that reminds him of the summertime because he believes the beach the is best place to be. Has a 4.0 GPA and intends to graduates with it if he can have any say in it.</p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -178,7 +178,7 @@ include_once($path);
 			</a>
 			<h3>Corey Conrad</h3>
 			<h6>Scroll Number: 591</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p></p>
 		</div>
 		<div class="col-md-3 col-xs-6 thumb member">
 			<a class="thumbnail" href="#">
@@ -186,7 +186,26 @@ include_once($path);
 			</a>
 			<h3>Josh Halaberda</h3>
 			<h6>Scroll Number: 592</h6>
-			<p>Two to four sentence piece of information about each member. Ask members to provide their own bio and copy paste inside the paragraph tags.</p>
+			<p>International Studies major and Spanish minor. Currently the Philanthropy Chairman for the Lambda-Upsilon Colony and the Vice President of Model Arab League at GSU. Also active in Mock Mediation Club as well as GSU's newly formed Rotoract Club.</p>
+		</div>
+	</div>
+	<hr />
+	<div class="row">
+		<div class="col-md-3 col-xs-6 thumb member">
+			<a class="thumbnail" href="#">
+				<img class="img-responsive" src="http://placehold.it/400x300" alt="">
+			</a>
+			<h3>Derrick Ramage</h3>
+			<h6>Scroll Number: ###</h6>
+			<p>Junior Economics major with a minor in Business Analytics, social chair of the Lambda-Upsilon Colony.  Outside of TKE Derrick serves as the President of the Eagles Economics Club and a member of Habitat for Humanity. Currently working on capstone project titled "Causes and Cures for poverty in the state of Georgia".</p>
+		</div>
+		<div class="col-md-3 col-xs-6 thumb member">
+			<a class="thumbnail" href="#">
+				<img class="img-responsive" src="http://placehold.it/400x300" alt="">
+			</a>
+			<h3>Jeffrey Toler</h3>
+			<h6>Scroll Number: ###</h6>
+			<p>Freshman economics major and Chinese minor with an emphasis on International Studies. Plays guitar and loves metal music.</p>
 		</div>
 	</div>
 	<hr />
