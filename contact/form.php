262d27e7c19e5b7c013216366fca1f7f929c9c2c
Alex Goff <alexander.r.goff@gmail.com>
2015-10-21 11:36:28 -0400

    hardened contact page

--- contact/form.php
+++ contact/form.php
@@ -1,10 +1,28 @@
 <?php
+//check to see if message has been filled out, if there is no message presumably it is not worth emailing
 if (!empty($_POST['contact-message'])) {
-    $name = $_POST['contact-name'];
-    $email = $_POST['contact-email'];
-    $phone = $_POST['contact-phone'];
-    $text = $_POST['contact-message'];
-	$topic = $_POST['contact-topic'];
+	
+	//check to see if contact name has been set, if so strip it and save as variable
+	if(isset($_POST['contact-name'])){
+		$name = strip_tags(trim($_POST['contact-name']));
+	}
+	//check to see if a valid email has been input, if so strip it and save
+	if(preg_match((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]),$_POST['contact-email'])){
+		$email = strip_tags(trim($_POST['email']));
+	}
+	//no regex for phones, too many possibilities
+	if(isset($_POST['contact-phone'])){
+		$phone = strip_tags(trim($_POST['contact-phone']));
+	}
+	//for contact message, do not strip HTML but convert it to an entity so links may be preserved
+	if(isset($_POST['contact-message'])){
+		$message = htmlentities(trim($_POST['contact-message']), ENT_NOQUOTES);
+	}
+	//hard coded variable in a page
+	if(isset($_POST['contact-topic'])){
+		$topic = strip_tags(trim($_POST['contact-topic']));
+	}
+	//if topic variable is not present, do not include
 	if (!empty($topic)) {
 		$subject = "Message from: ".$name." about ".$topic;
 	} else {
