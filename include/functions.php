3e7e74ce026970c244662a4dbe13f8a3e907365c
Alex Goff <alexander.r.goff@gmail.com>
2015-10-20 22:18:57 -0400

    pdo function

--- include/functions.php
+++ include/functions.php
@@ -4,9 +4,9 @@ function pdo_open() {
 	//define database informaiton
 	$dsn = 'mysql:dbname=tkegsuco_info;host=localhost;';
 	//database username, this will need to be changed
-	$username = 'tkegsuco_reader';
+	$username = 'tkegsuco_admin';
 	//database password, this will need to be changed
-	$password = 'a97004285e831b0dc';
+	$password = '!Ambd-688%';
 	
     //attempt to open connection, if connection is not available then give an error
     try {
